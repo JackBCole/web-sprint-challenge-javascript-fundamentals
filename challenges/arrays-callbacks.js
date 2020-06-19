2fc2a4af24768aced4d5859379efd0830f43cb90
JackBCole <64171526+JackBCole@users.noreply.github.com>
2020-06-19 11:43:53 -0500

    finished

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -58,10 +58,9 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-let populationTotal = 0;
-populationTotal = zooAnimals.reduce(function(accumulator, item){
+const populationTotal = zooAnimals.reduce(function(accumulator, item){
 return accumulator + item.population;
-});
+}, 0);
 console.log(populationTotal);
 
 
@@ -73,19 +72,29 @@ console.log(populationTotal);
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+const consume = function(a , b, cb){
+ return cb(a, b);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-
+const add = (a,b) => {
+  return a + b;
+};
+const multiply = (a,b) => {
+  return a*b;
+}
+const greeting = (first, last) => {
+  return `hello ${first} ${last} nice to meet you!`
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+ console.log(consume(2, 2, add)); // 4
+ console.log(consume(10, 16, multiply)); // 160
+ console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 
